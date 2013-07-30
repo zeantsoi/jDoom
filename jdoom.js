1f4330ffa03ad7b113371f8f93acac1841e59227
Zean <zean@ubuntu.(none)>
2013-07-29 23:56:36 -0600

    Starting to map out procedural script, alongside test HTML

--- jdoom.js
+++ jdoom.js
@@ -1,23 +1,52 @@
-// JavaScript Document
-// JavaScript Document
+/**
+*
+* @author Zean Tsoi http://www.zeantsoi.com/
+*
+* jDoom — A pure Javascript countdown timer
+*
+* The MIT License (MIT)
+*
+* Copyright (c) 2013 Zean Tsoi
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+*/
 
-$(document).ready(function() {
-  $('img.crest').delay(1000).fadeIn(2000);
-  $('#counter').delay(2000).fadeIn(3000);
-  
-  $('#counter').countdown({
-    image: 'img/digits.png',
-    //startTime: '01:12:12:00'
-    startTime: counter(diff)
-  });
-  
-  
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1; //January is 0!
-    
-window.setInterval(silly, 1000);
+/*
+Example Usage
+*/
+/*var e = document.getElementById('test');
+alert(e.innerHTML);
+e.innerHTML = 'dunno';*/
 
+
+var Doom = (function(options) {
+	var id = options.id || 'doom',
+		test = options.test || 'test';
+		
+  function begin() {
+		var e = document.getElementById(id);
+		e.innerHTML = test;
+  }
+  return {
+    begin: begin
+  };
 });
 
 function silly() {
